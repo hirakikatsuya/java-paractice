f26bd0310265f765ba81f44a96ee71844e989428
hirakikatsuya <h.k.56781075@gmail.com>
2023-01-23 00:55:20 +0900

    JavaBlackjack作成

--- BlackJack/src/net/sejuku/BlackjackClass.java
+++ BlackJack/src/net/sejuku/BlackjackClass.java
@@ -3,6 +3,7 @@ package net.sejuku;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class BlackjackClass {
 	public static void main(String[] args) {
@@ -19,6 +20,64 @@ public class BlackjackClass {
 
 		int deckCount = 4;
 		int playerHands = 2;
+
+		System.out.println("あなたの1枚目のカードは" + toDescription(player.get(0)));
+		System.out.println("ディーラーの1枚目のカードは" + toDescription(dealer.get(0)));
+		System.out.println("あなたの2枚目のカードは" + toDescription(player.get(1)));
+		System.out.println("ディーラーの2枚目のカードは秘密です。");
+
+		int playerPoint = sumPoint(player);
+		int dealerPoint = sumPoint(dealer);
+		System.out.println("あなたの現在のポイントは" + playerPoint + "です。");
+
+		while (true) {
+			System.out.println("カードを引きますか？Yes:y or No:n");
+			Scanner scan = new Scanner(System.in);
+			String str = scan.next();
+
+			if ("n".equals(str)) {
+				break;
+			} else if ("y".equals(str)) {
+				player.add(deck.get(deckCount));
+				deckCount++;
+				playerHands++;
+
+				System.out.println("あなたの" + playerHands + "枚目のカードは" + toDescription(player.get(playerHands - 1)));
+				playerPoint = sumPoint(player);
+				System.out.println("現在の合計は" + playerPoint);
+
+				if (isBusted(playerPoint)) {
+					System.out.println("残念、バーストしてしまいました。");
+					return;
+				}
+			} else {
+				System.out.println("あなたの入力は" + str + "です。yかnかを入力してください");
+			}
+		}
+
+		while (true) {
+			if (dealerPoint >= 17) {
+				break;
+			} else {
+				dealer.add(deck.get(deckCount));
+				deckCount++;
+				dealerPoint = sumPoint(dealer);
+				if (isBusted(dealerPoint)) {
+					System.out.println("ディーラーがバーストしました。あなたの勝ちです！");
+					return;
+				}
+			}
+		}
+		System.out.println("あなたのポイントは" + playerPoint);
+		System.out.println("ディーラーのポイントは" + dealerPoint);
+
+		if (playerPoint == dealerPoint) {
+			System.out.println("引き分けです");
+		} else if (playerPoint > dealerPoint) {
+			System.out.println("勝ちました！");
+		} else {
+			System.out.println("負けました・・・");
+		}
 	}
 
 	private static void shuffleDeck(List<Integer> deck) {
@@ -72,5 +131,28 @@ public class BlackjackClass {
 		String suit = toSuit(cardNumber);
 
 		return suit + "の" + rank;
+	}
+
+	private static int toPoint(int num) {
+		if (num == 11 || num == 12 || num == 13) {
+			num = 10;
+		}
+		return num;
+	}
+
+	private static int sumPoint(List<Integer> list) {
+		int sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum = sum + toPoint(toNumber(list.get(i)));
+		}
+		return sum;
+	}
+
+	private static boolean isBusted(int point) {
+		if (point <= 21) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
